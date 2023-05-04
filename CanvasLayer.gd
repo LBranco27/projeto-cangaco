b8f66cc86ffc2025e87fa8f0ac8a37afac5b02f6
pedronso <pedrocaetanoaraujopc@gmail.com>
2023-05-04 00:09:45 -0300

    scene change bug fix

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -3,8 +3,9 @@ extends CanvasLayer
 var simultaneous_scene = preload("res://scenes/3d/first_level.tscn").instantiate()
 
 func _on_button_start_pressed():
-	get_tree().get_root().add_child(simultaneous_scene)
-	get_node("/root/MainMenu").free()
+	get_tree().change_scene_to_file("res://scenes/3d/first_level.tscn")
+	#get_tree().get_root().add_child(simultaneous_scene)
+	#get_node("/root/MainMenu").free()
 
 
 func _on_button_exit_pressed():
